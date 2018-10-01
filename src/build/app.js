8082f3c675e39046202d3feb7389d4fbec6039e1
Yuji Yamaguchi <yuji.yamaguchi24@gmail.com>
2018-10-01 22:42:39 +0900

    upgrade

---
+++ src/build/app.js
@@ -0,0 +1,30 @@
+const request = require('request')
+const unzipper = require('unzipper')
+const Encoding = require('encoding-japanese')
+const csv = require('csvtojson')
+const fs = require('fs-extra')
+
+unzipper.Open.url(request, 'https://www.post.japanpost.jp/zipcode/dl/kogaki/zip/ken_all.zip')
+  .then((d) => {
+    var file = d.files[0]
+    return file.buffer()
+  })
+  .then((buffer) => {
+    const utf8String = Encoding.convert(buffer, {
+      from: 'SJIS',
+      to: 'UNICODE',
+      type: 'string'
+    })
+
+    csv({
+      noheader: true,
+      headers: ['_', '_', 'zipcode', 'state_kana', 'city_kana', 'address_kana', 'state', 'city', 'address', '_', '_', '_', '_', '_', '_'],
+      colParser: {
+        '_': 'omit',
+      },
+    })
+    .fromString(utf8String)
+    .then((jsonObj) => {
+      fs.outputJsonSync('./dist/zipcode.json', jsonObj)
+    })
+  })
